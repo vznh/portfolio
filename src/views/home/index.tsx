0c33d826eb2a21f03133f28ce6ea774639e57934
Jason Son <jasonson2004@gmail.com>
2024-07-09 05:12:42 -0700

    initial commit

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -19,9 +19,9 @@ export const HomeView: FC<HomeProps> = () => {
         "h-full w-full"
       )}>
         <h1><b><a>Jason Son</a></b></h1>
-        <span className="text-lg"><i>Co-founder of <a href="https://tokn.so"><u>Tokn</u></a>, a Web3 social token analytics platform, and <a href="https://landing-lime-five.vercel.app/"><u>Polyglot</u></a>, a one-stop shop to learn any language you want.</i></span>
+        <span className="text-lg"><i>Software <b>@</b> Apple, co-founder of <a href="https://tokn.so"><u>Tokn</u></a>, a Web3 social token analytics platform, and <a href="https://landing-lime-five.vercel.app/"><u>Polyglot</u></a>, a one-stop shop to learn any language you want.</i></span>
     
-        <h1><b>More platforms</b></h1>
+        <h1><b>Related links</b></h1>
         <div className="flex flex-row">
           <a href="https://www.instagram.com/jsonvinh/" className="text-lg"><i><u>Instagram</u></i></a>
           <a href="https://x.com/vivivinh" className="text-lg"><i><u>X.com</u></i></a>
