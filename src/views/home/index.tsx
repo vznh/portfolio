d20cacd71187e4154a217de9d80f73843b8df0eb
Jason Son <jasonson2004@gmail.com>
2024-07-20 15:53:14 -0700

    add: fixed build error

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -22,7 +22,7 @@ export const HomeView: FC<HomeProps> = () => {
         <span className="text-lg"><i>Software <b>@</b> Apple, co-founder of <a href="https://tokn.so"><u>Tokn</u></a>, a Web3 social token analytics platform, and <a href="https://landing-lime-five.vercel.app/"><u>Polyglot</u></a>, a one-stop shop to learn any language you want.</i></span>
         <span className="text-lg"><i>Board on <a href="https://www.instagram.com/ucscsase/"><u>SASE</u></a>, <a href="https://tally.so/r/3E0Y62"><u>Dev Club</u></a>, <a href="https://discord.gg/eD9RN753zt"><u>Lego Club</u></a>.</i></span>
         <br />
-        <span className="text-lg"><i>If you're interested in me or my work, <a href="https://cal.com/hobin/15min"><u>book a call.</u></a></i></span>
+        <span className="text-lg"><i>If you&apos;re interested in me or my work, <a href="https://cal.com/hobin/15min"><u>book a call.</u></a></i></span>
         <h1><b>Related links</b></h1>
         <div className="flex flex-col">
           <ul>
