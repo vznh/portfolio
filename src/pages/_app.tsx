af043302eec4c3746211aa09f846c639115183b8
Jason Son <jasonson2004@gmail.com>
2024-07-24 06:01:46 -0700

    add: analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ContentContainer } from "@/components/templates/ContentContainer";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ContentContainer>
       <Component {...pageProps} />
+      <Analytics />
     </ContentContainer>
   );
 }
