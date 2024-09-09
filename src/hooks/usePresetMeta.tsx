3940e5a8887ca581469d68acaa8eb954e46bba2a
Jason Son <jasonson2004@gmail.com>
2024-09-08 21:12:36 -0700

    attempt 2: fix open graph tags

--- src/hooks/usePresetMeta.tsx
+++ src/hooks/usePresetMeta.tsx
@@ -2,6 +2,8 @@
 import Head from 'next/head';
 
 export function UsePresetMeta() {
+  const siteUrl = process.env.NEXT_PUBLIC_URL || 'https://www.hobin.dev';
+
   return (
     <Head>
       <title>Jason Son - Personal</title>
@@ -10,14 +12,15 @@ export function UsePresetMeta() {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta property="og:title" content="Jason Son" />
       <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Jason Son - Personal" />
       <meta property="og:description" content="Programmer, serial chiller." />
-      <meta property="og:url" content="https://www.hobin.dev" />
-      <meta property="og:image" content="/images/cover-image.png" />
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:image" content={`${siteUrl}/images/cover-image.png`} />
       <link rel="icon" href="/images/favicon.ico" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Jason Son - Person" />
       <meta name="twitter:description" content="Programmer, serial chiller." />
-      <meta name="twitter:image" content="/images/cover-image.png" />
+      <meta name="twitter:image" content={`${siteUrl}/images/cover-image.png`} />
     </Head>
   );
 }
