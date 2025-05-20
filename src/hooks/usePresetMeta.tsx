6229c82a24a2faa11f1d3c53b1ce89109fd0c192
Jason Son <jasonson2004@gmail.com>
2025-05-20 15:57:39 -0700

    mod: changed images to match new imgs

--- src/hooks/usePresetMeta.tsx
+++ src/hooks/usePresetMeta.tsx
@@ -6,21 +6,21 @@ export function UsePresetMeta() {
 
   return (
     <Head>
-      <title>Jason Son - Personal</title>
-      <meta name="description" content="Programmer, and occasional chiller." />
-      <meta name="keywords" content="polyglot, ceo, student, biology and cs at ucsc" />
+      <title>JASON SON</title>
+      <meta name="description" content="Engineering pursuing alignment." />
+      <meta name="keywords" content="" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta property="og:title" content="Jason Son" />
       <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="Jason Son - Personal" />
-      <meta property="og:description" content="Programmer, serial chiller." />
+      <meta property="og:site_name" content="JASON SON" />
+      <meta property="og:description" content="Engineer pursuing alignment." />
       <meta property="og:url" content={siteUrl} />
-      <meta property="og:image" content={`${siteUrl}/images/cover-image.png`} />
+      <meta property="og:image" content={`${siteUrl}/images/cover.png`} />
       <link rel="icon" href="/images/favicon.ico" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Jason Son - Person" />
-      <meta name="twitter:description" content="Programmer, serial chiller." />
-      <meta name="twitter:image" content={`${siteUrl}/images/cover-image.png`} />
+      <meta name="twitter:description" content="Engineer pursuing alignment." />
+      <meta name="twitter:image" content={`${siteUrl}/images/cover.png`} />
     </Head>
   );
 }
