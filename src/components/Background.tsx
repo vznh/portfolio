1e0fb9c8f366033b618264e574ca6e8a9401e4e5
vznh <65467162+vznh@users.noreply.github.com>
2025-06-20 17:46:39 -0700

    add: new textile background

---
+++ src/components/Background.tsx
@@ -0,0 +1,20 @@
+// components/Background
+import Image from "next/image";
+
+const Background = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full h-full z-[-1]">
+      <div className="relative w-full h-full">
+        <Image 
+          src="/images/bg/rs.png" 
+          alt="background" 
+          className="opacity-30" 
+          fill 
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Background;
