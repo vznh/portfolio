73f40b5f76cdb45bc4836cc46124887826302de9
vznh <65467162+vznh@users.noreply.github.com>
2025-06-28 16:40:16 -0700

    add: new backgrounds, setup for light

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,15 +1,15 @@
 // components/Background
 import Image from "next/image";
 
-const Background = () => {
+const Background: React.FC<{ dark: boolean }> = ({ dark }) => {
   return (
     <div className="absolute top-0 left-0 w-full h-full z-[-1]">
       <div className="relative w-full h-full">
-        <Image 
-          src="/images/bg/rs.png" 
-          alt="background" 
-          className="opacity-45" 
-          fill 
+        <Image
+          src={ dark ? "/images/bg/tex.png" : "/images/bg/light.jpg" }
+          alt="background"
+          className="opacity-50"
+          fill
           style={{ objectFit: 'cover' }}
         />
       </div>
