22d610a54d4e4ec772d17a5a49a489b9bc22d58d
vznh <65467162+vznh@users.noreply.github.com>
2026-09-07 19:20:32 -0400

    Double the gap between listed subheadings

--- src/components/cells/OpenedSectionsCell.tsx
+++ src/components/cells/OpenedSectionsCell.tsx
@@ -19,7 +19,7 @@ export function OpenedSectionsCell() {
     .filter((section) => section?.heading);
 
   return (
-    <ul className="flex flex-col gap-1">
+    <ul className="flex flex-col gap-2">
       {opened.map((section) => (
         <li key={section!.id} className="animate-fade-in">
           <button
