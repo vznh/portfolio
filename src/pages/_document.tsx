f8f6b599a2a556ab5d5cbe9c4c49f75aeb97d39c
vznh <65467162+vznh@users.noreply.github.com>
2026-09-08 04:12:16 -0400

    Preload the Schengen A font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/schengen-a-regular.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
